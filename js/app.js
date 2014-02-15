557e4500b8783e8cf3e4908a99c46b44977ed8a9
Caleb Larsen <cmlarsen@gmail.com>
2014-02-15 09:08:06 -0500

    Setting type, tweaking nav

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 $(document).foundation();
+
+$(document).ready(function(){
+
+	// Under line Nav on hover.
+	var $navLinks = $("nav a:not(a.button):not(a.icon)");
+	$navLinks.hover(function(){
+	
+		// $(this).toggleClass("active");
+		// console.log("howdy", this);
+	});
+
+});
